f79272f8621cde7b054fa497ad3a686463d083ce
n0tabumblebee <n0tabumblebee@yahoo.com>
2016-12-02 19:52:37 -0800

    Update mainBot.php

--- mainBot.php
+++ mainBot.php
@@ -6,7 +6,7 @@ include('./httpful.phar');
 include('./functions.php');
 
 // this is found at http://dev.groupme.com/bots
-$bot_token = "the main bot id";
+$bot_token =  "a8555ce2080375ffb31262b66d";
 
 include('./index.php');
 
